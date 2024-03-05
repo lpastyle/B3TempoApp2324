6cea0ed4b932295b7be0947bb8a0b85ed23ab180
lpastyle <lpastorelli@lpastyle.com>
2024-03-05 11:28:33 +0100

    Added TempoColor enum

---
+++ app/src/main/java/com/example/b3tempoapp2324/TempoColor.java
@@ -0,0 +1,17 @@
+package com.example.b3tempoapp2324;
+
+public enum TempoColor {
+    RED(R.color.tempo_red_day_bg, R.string.tempo_red_color_text),
+    WHITE(R.color.tempo_white_day_bg, R.string.tempo_white_color_text),
+    BLUE(R.color.tempo_blue_day_bg, R.string.tempo_blue_color_text),
+    UNKNOWN(R.color.tempo_undecided_day_bg, R.string.tempo_undecided_color_text);
+
+    private final int colorResId;
+    private final int stringResId;
+
+    // Ctor
+    TempoColor(int colorResId, int stringResId) {
+        this.colorResId = colorResId;
+        this.stringResId = stringResId;
+    }
+}
